ef1c8ee066fccde73dd5e158229aaa13d5d0b297
Rebeca <becaporto31@gmail.com>
2024-10-11 14:11:30 -0300

    desafio aula autenticacao

--- interface/src/routes.jsx
+++ interface/src/routes.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoutes from './components/Rotas/ProtectedRoutes';
 import Home from './pages/Home/Home';
 import Pessoa from './pages/Pessoa/Pessoa';
+import Login from './pages/Login/Login';
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Rotas principais */}
                 <Route path="/" element={<Home />} />
-                <Route path='/pessoas' element={<Pessoa />} />
+                <Route path="/login" element={<Login />} />
+
+                {/* Rotas protegidas */}
+                <Route path='/pessoas' element={<ProtectedRoutes element={Pessoa} />} />
             </Routes>
         </BrowserRouter>
     );
